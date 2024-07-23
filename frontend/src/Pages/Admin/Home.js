dc73d052819fbc7fd12fc14f7f8f39460faefaeb
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2024-07-23 12:41:09 +0530

    Making Admin Home Page

---
+++ frontend/src/Pages/Admin/Home.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Logs from "../../Components/Admin/Logs";
+import Enquiry from "./Enquiry";
+
+const Home = ({ name , id }) => {
+    const [mode, setMode] = useState(0)
+    return <div>
+        <h1>Welcome, {name}</h1>
+        <h3>ID: {id} (Admin)</h3>
+
+        <button onClick={() => setMode(1)}>View Statistics</button>
+        <button onClick={() => setMode(2)}>Enquire</button>
+        <button onClick={() => setMode(3)}>Generate Logs</button>
+
+        <div>
+                {mode === 1 ? (
+                    <></>
+                    // <UploadAttendance name={name} id={id} /
+                ) : mode === 2 ? (
+                    <Enquiry />
+                ) : mode === 3 ? (
+                    <Logs name={name} id={id} />
+                ) : <></>}
+            </div>
+    </div>;
+};
+
+export default Home;
